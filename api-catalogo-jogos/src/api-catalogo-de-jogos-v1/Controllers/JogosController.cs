64e56c519ca90126ae9229e973c9a455b5d71876
cyrogabrielcc <cyrocunhacg@gmail.com>
2023-09-26 21:48:59 -0300

    iniciando parte 3

--- api-catalogo-jogos/src/api-catalogo-de-jogos-v1/Controllers/JogosController.cs
+++ api-catalogo-jogos/src/api-catalogo-de-jogos-v1/Controllers/JogosController.cs
@@ -11,40 +11,56 @@ namespace api_catalogo_de_jogos.Controllers
     [Route("api/[controller]")]
     public class JogosController : ControllerBase
     {
+
         [HttpGet]
         public async Task<ActionResult<List<object>>> GetAllJogos()
         {
             return Ok();
         }
 
-         [HttpGet("{JogoId:guid}")]
+        /*---------------------------------------------------------------*/
+        [HttpGet("{JogoId:guid}")]
         public async Task<ActionResult<object>> GetJogoByID(Guid idJogo)
         {
             return Ok();
         }
+       
+       
+       
+        /*---------------------------------------------------------------*/
         [HttpPost]
         public async Task<ActionResult<object>> CreateNewJogo()
         {
             return Ok();
         }
 
+        
+        
+        
+        /*---------------------------------------------------------------*/
         [HttpPut("{JogoId:guid")]
         public async Task<ActionResult<object>> ChangeJogo(Guid idJogo)
         {
             return Ok();
         }
-
+        
+        
+        /*---------------------------------------------------------------*/
         [HttpPatch("{JogoId:guid}/preco/{preco:double}")]
         public async Task<ActionResult<object>> ChangeItemJogo(Guid idJogo, double jogo)
         {
             return Ok();
         }
 
-        /*
-        [HttpDelete("{JogoId:guid}")]
+        
+        
+        
+       /*---------------------------------------------------------------*/
+        
+       /* [HttpDelete("{JogoId:guid}")]
         public async Task<AcceptedResult> DeleteJogo(Guid id)
         {
-            return 
+            return Ok();
         }
         */
     }
---
+++ api-catalogo-jogos/src/api-catalogo-de-jogos-v1/InputModel/JogoInputModel.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api_catalogo_de_jogos.InputModel
+{
+    public class JogoInputModel
+    {
+        /*_______________________#####_______________________*/
+        [Required]
+        [StringLength(100, MinimumLength = 2, ErrorMessage = "Deve ter entre 2 e 100")]
+        public string Nome {get; set;}
+        
+       
+       /*_______________________#####_______________________*/
+        [Required]
+        [StringLength(100, MinimumLength = 2, ErrorMessage = "Deve ter entre 2 e 100")]
+        public string Produtora {get; set;}
+      
+        /*_______________________#####_______________________*/
+        [Required]
+        [Range(1,1000, ErrorMessage = "mínimo de 50 e máximo de 1000")]
+        public double Preco {get; set;}
+    }
+}
