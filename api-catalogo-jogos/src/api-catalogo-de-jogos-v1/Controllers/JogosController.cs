c5486d1dae688d31116d0c3bfafb99820d0933f6
cyrogabrielcc <cyrocunhacg@gmail.com>
2023-09-26 21:28:51 -0300

    iniciando parte 3

--- api-catalogo-jogos/src/api-catalogo-de-jogos-v1/Controllers/JogosController.cs
+++ api-catalogo-jogos/src/api-catalogo-de-jogos-v1/Controllers/JogosController.cs
@@ -1,3 +1,4 @@
+using System.Net;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,6 +11,41 @@ namespace api_catalogo_de_jogos.Controllers
     [Route("api/[controller]")]
     public class JogosController : ControllerBase
     {
-        
+        [HttpGet]
+        public async Task<ActionResult<List<object>>> GetAllJogos()
+        {
+            return Ok();
+        }
+
+         [HttpGet("{JogoId:guid}")]
+        public async Task<ActionResult<object>> GetJogoByID(Guid idJogo)
+        {
+            return Ok();
+        }
+        [HttpPost]
+        public async Task<ActionResult<object>> CreateNewJogo()
+        {
+            return Ok();
+        }
+
+        [HttpPut("{JogoId:guid")]
+        public async Task<ActionResult<object>> ChangeJogo(Guid idJogo)
+        {
+            return Ok();
+        }
+
+        [HttpPatch("{JogoId:guid}/preco/{preco:double}")]
+        public async Task<ActionResult<object>> ChangeItemJogo(Guid idJogo, double jogo)
+        {
+            return Ok();
+        }
+
+        /*
+        [HttpDelete("{JogoId:guid}")]
+        public async Task<AcceptedResult> DeleteJogo(Guid id)
+        {
+            return 
+        }
+        */
     }
 }
